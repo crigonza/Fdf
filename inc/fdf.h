78c96ced857ef408762dbff6049244f0df8d4bf0
Cristobal Gonzalez aguilar <crigonza@c2r9s3.42malaga.com>
2022-09-23 19:27:11 +0200

    42

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,69 +6,68 @@
 /*   By: crigonza <crigonza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:40:19 by crigonza          #+#    #+#             */
-/*   Updated: 2022/09/22 18:58:02 by crigonza         ###   ########.fr       */
+/*   Updated: 2022/09/23 18:52:13 by crigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
-#define FDF_H
+# define FDF_H
 
-#include "../libft/libft.h"
-#include "../GNL/get_next_line.h"
-#include "../MLX42/include/MLX42/MLX42.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <math.h>
+# include "../GNL/get_next_line.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-#define WIN_H 1080
-#define WIN_W 1920
+# define WIN_H 1080
+# define WIN_W 1920
 
 typedef struct s_coords
 {
-    int     x;
-    int     y;
-    int     z;
-}       t_coords;
+	int			x;
+	int			y;
+	int			z;
+}				t_coords;
 
 typedef struct s_fdf
 {
-    int        **map;
-    int         height;
-    int         width;
-    int         color;
-    float         angle;
-    float         alpha;
-    float        beta;
-    float        gamma;
-    int         scale;
-    int         shiftx;
-    int         shifty;
-    mlx_t        *mlx;
-    mlx_image_t       *img;
-}   t_fdf;
+	int			**map;
+	int			height;
+	int			width;
+	int			color;
+	float		alpha;
+	float		beta;
+	float		gamma;
+	int			scale;
+	int			shiftx;
+	int			shifty;
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+}				t_fdf;
 
 //parse_file.c
-int get_width(char *file);
-int get_height(char *file);
-int	**allocate_map(int height, int width);
-void    parser(t_fdf *fdf, char *file);
+int				get_width(char *file);
+int				get_height(char *file);
+int				**allocate_map(int height, int width);
+void			parser(t_fdf *fdf, char *file);
 //set_map.c
-int    get_color(t_coords *pt);
-void    rotation(t_coords *coord, t_fdf *fdf);
-void    parallel_view(t_coords *coords, int z);
-void    isometric_view(t_coords *coords);
-void    set_point(t_coords *pt1, t_coords *pt2, t_fdf *fdf);
+int				get_color(t_coords *pt);
+void			rotation(t_coords *coord, t_fdf *fdf);
+void			parallel_view(t_coords *coords, int z);
+void			isometric_view(t_coords *coords);
+void			set_point(t_coords *pt1, t_coords *pt2, t_fdf *fdf);
 //draw_map.c
-t_coords    set_coords(int x, int y, t_fdf *fdf);
-void    draw_lines(t_fdf *fdf);
-void    line_algorithm(t_fdf *fdf, t_coords pt1, t_coords pt2);
+t_coords		set_coords(int x, int y, t_fdf *fdf);
+void			draw_lines(t_fdf *fdf);
+void			line_algorithm(t_fdf *fdf, t_coords pt1, t_coords pt2);
 //utils.c
-int     abs_val(int num);
-int     check_points(int x, int x2);
-void    map_error(void);
+int				abs_val(int num);
+int				check_points(int x, int x2);
+void			map_error(void);
 //fdf.c
-int main(int argc, char **argv);
+int				main(int argc, char **argv);
 
 #endif
--- src/fdf.c
+++ src/fdf.c
@@ -6,109 +6,131 @@
 /*   By: crigonza <crigonza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 19:25:36 by crigonza          #+#    #+#             */
-/*   Updated: 2022/09/22 21:21:05 by crigonza         ###   ########.fr       */
+/*   Updated: 2022/09/23 18:58:11 by crigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
 
-void    control_keys(mlx_key_data_t keydata, t_fdf *fdf)
+void	control_keys(mlx_key_data_t keydata, t_fdf *fdf)
 {
-    /* if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-        mlx_close_window(param); */
-    if (keydata.key == MLX_KEY_Q && keydata.action == MLX_PRESS)
-        fdf->alpha += 0.05;
-    if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
-        fdf->alpha -= 0.05;
-    if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
-        fdf->beta += 0.05;
-    if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
-        fdf->beta -= 0.05;
-    if (keydata.key == MLX_KEY_Z && keydata.action == MLX_PRESS)
-        fdf->gamma += 0.05;
-    if (keydata.key == MLX_KEY_X && keydata.action == MLX_PRESS)
-        fdf->gamma -= 0.05;
-    if (keydata.key == MLX_KEY_O && keydata.action == MLX_PRESS)    
-        fdf->scale += 1;
-    if (keydata.key == MLX_KEY_P && keydata.action == MLX_PRESS)    
-        fdf->scale -= 1;
-    if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS)    
-        fdf->shifty -= 10;
-    if (keydata.key == MLX_KEY_DOWN && keydata.action == MLX_PRESS)    
-        fdf->shifty += 10;
-    if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)    
-        fdf->shiftx -= 10;
-    if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)    
-        fdf->shiftx += 10;
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+		mlx_close_window(fdf->mlx);
+	if (keydata.key == MLX_KEY_Q && keydata.action == MLX_PRESS)
+		fdf->alpha += 0.05;
+	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
+		fdf->alpha -= 0.05;
+	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
+		fdf->beta += 0.05;
+	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
+		fdf->beta -= 0.05;
+	if (keydata.key == MLX_KEY_Z && keydata.action == MLX_PRESS)
+		fdf->gamma += 0.05;
+	if (keydata.key == MLX_KEY_X && keydata.action == MLX_PRESS)
+		fdf->gamma -= 0.05;
+	if (keydata.key == MLX_KEY_O && keydata.action == MLX_PRESS)
+		fdf->scale += 1;
+	if (keydata.key == MLX_KEY_P && keydata.action == MLX_PRESS)
+		fdf->scale -= 1;
+	if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS)
+		fdf->shifty -= 10;
+	if (keydata.key == MLX_KEY_DOWN && keydata.action == MLX_PRESS)
+		fdf->shifty += 10;
+	if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)
+		fdf->shiftx -= 10;
+	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
+		fdf->shiftx += 10;
 }
 
-void    background(t_fdf *fdf)
+void	background(t_fdf *fdf)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    x = 0;
-    while (x < WIN_H)
-    {
-        y = 0;
-        while (y < WIN_W)
-        {
-            mlx_put_pixel(fdf->img, y, x, 0x1f2124FF);
-            y++;
-        }
-        x++;
-    }
+	x = 0;
+	while (x < WIN_H)
+	{
+		y = 0;
+		while (y < WIN_W)
+		{
+			mlx_put_pixel(fdf->img, y, x, 0x1f2124FF);
+			y++;
+		}
+		x++;
+	}
 }
 
-void    initialize(t_fdf *fdf)
+void	initialize(t_fdf *fdf)
 {
-    fdf->angle = M_PI / 3;
-    fdf->scale = WIN_W / (fdf->height * 1.5);
-    fdf->shiftx = WIN_H / 2;
-    fdf->alpha = 0;
-    fdf->beta = 0;
-    fdf->gamma = 0;
+	if (fdf->width > fdf->height * 1.5)
+	{
+		fdf->scale = (int)round(WIN_W / (fdf->height * 2.5));
+		fdf->shiftx = WIN_W / 2.4;
+	}
+	else
+	{
+		fdf->scale = (int)round(WIN_W / (fdf->height * 1.7));
+		fdf->shiftx = WIN_W / 2.1;
+	}
+	fdf->shifty = WIN_H / 9;
+	fdf->alpha = 0;
+	fdf->beta = 0;
+	fdf->gamma = 0;
 }
 
-int main(int argc, char **argv)
+void	controls_text(t_fdf *fdf)
 {
-    t_fdf *fdf;
-    //mlx_image_t *fdf->img;
-    int i = 0;
-    int j = 0;
+	mlx_put_string(fdf->mlx, "PRESS SPACE FOR CHANGE PERSPECTIVE", 50, 30);
+	mlx_put_string(fdf->mlx, "PRESS ARROW KEYS TO MOVE MAP", 50, 50);
+	mlx_put_string(fdf->mlx, "PRESS O-P KEYS TO ZOOM IN-OUT", 50, 70);
+	mlx_put_string(fdf->mlx, "ROTATION:", 50, 90);
+	mlx_put_string(fdf->mlx, "PRESS Q-W FOR X AXIS", 50, 110);
+	mlx_put_string(fdf->mlx, "PRESS A-W FOR Y AXIS", 50, 130);
+	mlx_put_string(fdf->mlx, "PRESS Z-X FOR Z AXIS", 50, 150);
+    mlx_put_string(fdf->mlx, "PRESS ESC TO EXIT", 50, 170);
+}
 
-    fdf = (t_fdf*)malloc(sizeof(t_fdf));
-    if (argc != 2)
-    {
-        ft_putendl_fd("Invalid arguments number", 2);
-        exit(0);
-    }
-    parser(fdf, argv[1]);
-    printf("%d\n", fdf->width);
-    printf("%d\n", fdf->height);
-    initialize(fdf);
-    fdf->mlx = mlx_init(WIN_W, WIN_H, "Fdf", true);
-    fdf->img = mlx_new_image(fdf->mlx, WIN_W, WIN_H);
-    mlx_image_to_window(fdf->mlx, fdf->img, 0, 0);
-    draw_lines(fdf);
-    //hook (fdf);
-    mlx_key_hook(fdf->mlx, &control_keys, fdf);
-    /* while (i < fdf->height)
+int	main(int argc, char **argv)
+{
+	t_fdf *fdf;
+	//mlx_image_t *fdf->img;
+	int i = 0;
+	int j = 0;
+
+	fdf = (t_fdf *)malloc(sizeof(t_fdf));
+	if (argc != 2)
+	{
+		ft_putendl_fd("Invalid arguments number", 2);
+		exit(0);
+	}
+	parser(fdf, argv[1]);
+	printf("%d\n", fdf->width);
+	printf("%d\n", fdf->height);
+	initialize(fdf);
+	fdf->mlx = mlx_init(WIN_W, WIN_H, "Fdf", true);
+	fdf->img = mlx_new_image(fdf->mlx, WIN_W, WIN_H);
+	mlx_image_to_window(fdf->mlx, fdf->img, 0, 0);
+	draw_lines(fdf);
+	controls_text(fdf);
+	//hook (fdf);
+	mlx_key_hook(fdf->mlx, &control_keys, fdf);
+	/* while (i < fdf->height)
     {
         j = 0;
         while(j < fdf->width)
         {
-            mlx_put_pixel(fdf->img, (WIN_W /fdf->width) * j ,(WIN_H / fdf->height) * i, 0xFFFFFFFF);
+            mlx_put_pixel(fdf->img, (WIN_W /fdf->width) * j ,(WIN_H
+			/ fdf->height) * i, 0xFFFFFFFF);
             j++;
         }
         i++;
     } */
-    //line_algorithm(fdf, set_coords(1, 1), set_coords(1, 2));
-    mlx_loop_hook (fdf->mlx, &draw_lines, fdf);
-    mlx_loop(fdf->mlx);
-    mlx_delete_image(fdf->mlx, fdf->img);
-    mlx_terminate(fdf->mlx);
-    /* while (i < fdf->height)
+	//line_algorithm(fdf, set_coords(1, 1), set_coords(1, 2));
+	mlx_loop_hook(fdf->mlx, &draw_lines, fdf);
+	mlx_loop(fdf->mlx);
+	mlx_delete_image(fdf->mlx, fdf->img);
+	mlx_terminate(fdf->mlx);
+	/* while (i < fdf->height)
     {
         j = 0;
         while (j < fdf->width)
@@ -119,5 +141,5 @@ int main(int argc, char **argv)
         printf("\n");
         i++;
     } */
-    return(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
