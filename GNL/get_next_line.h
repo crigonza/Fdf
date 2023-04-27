7bf88a2e8da2fa9cfa18988224731c66c66150cd
Cristobal Gonzalez <crigonza@Mac-mini-de-Cristobal.local>
2023-04-27 08:32:00 +0200

    last

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -1,30 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: crigonza <crigonza@student.42malaga.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/19 20:47:49 by crigonza          #+#    #+#             */
-/*   Updated: 2023/04/19 20:47:57 by crigonza         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
-# endif
+# define BUFFER_SIZE 1000
+#endif
 
 char	*get_next_line(int fd);
-char	*ft_read_to_left_str(int fd, char *left_str);
-char	*ft_strchr_(char *s, int c);
-char	*ft_strjoin_(char *left_str, char *buff);
-size_t	ft_strlen_(char *s);
-char	*ft_get_line(char *left_str);
-char	*ft_new_left_str(char *left_str);
 
 #endif
